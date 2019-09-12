caabac08b1ba15406722c04eeca96a569aa6121b
Sam <samuel8chang@gmail.com>
2019-09-12 09:46:10 -0700

    lint

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import styled from "@emotion/styled";
-import React, { useState, useEffect } from "react";
-import { css, jsx } from "@emotion/core";
+import { useState, useEffect } from "react";
+import { jsx } from "@emotion/core";
 import { throttle } from "throttle-debounce";
 
 const ScrollToTop = styled.div`
