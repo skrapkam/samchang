ec3ee67b1c5fccd6273086a7bd1f8034c02a7d0d
skrapkam <samuel8chang@gmail.com>
2019-09-21 18:22:20 -0700

    Polyfill

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
     `gatsby-plugin-catch-links`,
+    `gatsby-plugin-polyfill-io`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
