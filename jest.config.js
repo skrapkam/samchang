00482b77e4768d4b632fe29b204536c1ff2bf128
Sam Chang <samuel8chang@gmail.com>
2025-06-09 14:57:58 -0700

    Configure ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  testMatch: ['**/tests/**/*.(test|spec).ts?(x)']
+  testMatch: ['**/tests/**/*.test.ts?(x)', '**/tests/**/*.spec.ts?(x)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json'
+    }
+  }
 };
