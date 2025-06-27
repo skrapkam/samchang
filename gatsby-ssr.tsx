78d0cea3fbf942166ffd1ac878dc9b2103f10af1
skrapkam <samuel8chang@gmail.com>
2025-06-27 11:30:45 -0700

    Error again again

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { PortfolioChatBot, ChatProvider } from "./src/components/ChatBot";
-import SecretBypassToggle from "./src/components/Bypass/SecretBypassToggle";
+import { ChatProvider } from "./src/components/ChatBot";
 
 export const wrapRootElement = ({ element }: { element: React.ReactNode }) => (
-  React.createElement(ChatProvider, null,
-    element,
-    React.createElement(PortfolioChatBot, null),
-    React.createElement(SecretBypassToggle, null)
-  )
+  React.createElement(ChatProvider, null, element)
 );
